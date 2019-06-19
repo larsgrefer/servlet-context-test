55a931a4c65ade748f096002961018811a120255
Lars Grefer <github@larsgrefer.de>
2019-06-19 02:41:36 +0200

    Add jib for war apps

--- src/main/java/de/larsgrefer/test/App.java
+++ src/main/java/de/larsgrefer/test/App.java
@@ -3,10 +3,11 @@ package de.larsgrefer.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
